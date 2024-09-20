6c89157077860c4fa653d3ff1a276bc76d3efb63
sebahernandez <sebaprogramer@gmail.com>
2024-09-20 19:26:27 -0300

    hashrouter activo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AppProvider } from "./context/AppContext";
 import "./index.css";
@@ -8,9 +8,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProvider>
-      <Router>
+      <HashRouter>
         <App />
-      </Router>
+      </HashRouter>
     </AppProvider>
   </React.StrictMode>
 );
