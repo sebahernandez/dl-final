4a217a0fecbcd1a75025d2a5736d1c9ef973aa94
sebahernandez <sebaprogramer@gmail.com>
2024-10-01 10:25:22 -0300

    ajuste loading

--- backend/src/server/middleware/authToken.js
+++ backend/src/server/middleware/authToken.js
@@ -1,34 +1,23 @@
-import { jwtVerify } from "../../utils/jwt.js"; // Asegúrate de que jwtVerify funcione correctamente
-
+import { jwtVerify } from "../../utils/jwt.js";
 export const authToken = (req, res, next) => {
-  // Obtener el encabezado Authorization
-
   const authorization = req.header("Authorization");
-
-  // Verificar si el encabezado Authorization está presente
   if (!authorization) {
     return res
       .status(401)
       .json({ message: "Usuario no autorizado, falta el token" });
   }
-
-  // Dividir el encabezado Authorization en "Bearer" y el token
   const [bearer, token] = authorization.split(" ");
 
-  // Verificar si el formato es correcto
   if (bearer !== "Bearer" || !token) {
     return res.status(401).json({ message: "Token mal formado" });
   }
 
   try {
-    // Verificar el token y obtener los datos decodificados
-    const decoded = jwtVerify(token); // Asegúrate de que jwtVerify devuelva los datos decodificados
-    req.user = decoded; // Asignar el usuario decodificado al request
+    const decoded = jwtVerify(token);
+    req.user = decoded;
 
-    // Continuar con la siguiente función (setUser)
     next();
   } catch (error) {
-    // Si el token no es válido, devolver un error
     return res.status(401).json({ message: "Token inválido o expirado" });
   }
 };
